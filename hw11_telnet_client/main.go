9127571e8ed104adf1b6efebb1df21c88cf109e7
Artur Khasanov <ah@self.team>
2024-10-22 23:34:35 +0300

    Done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,58 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", time.Second*10, "timeout")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		log.Fatalln("Usage: go-telnet --timeout=10 <host> <port>")
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := host + ":" + port
+
+	client := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+
+	if err := client.Connect(); err != nil {
+		fmt.Printf("Cannot connect to %s: %v\n", address, err)
+	}
+
+	defer func() {
+		if err := client.Close(); err != nil {
+			fmt.Printf("Cannot close connection: %v\n", err)
+		}
+	}()
+
+	log.Printf("...Connected to %s\n", address)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	go func() {
+		if err := client.Send(); err != nil {
+			log.Fatalln(err)
+		}
+		cancel()
+	}()
+
+	go func() {
+		if err := client.Receive(); err != nil {
+			log.Fatalln(err)
+		}
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
