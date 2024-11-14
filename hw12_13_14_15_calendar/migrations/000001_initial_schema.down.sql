c309148b9c51409ed2eaa2ae477664a14ca874c8
Artur Khasanov <ah@self.team>
2024-11-14 20:06:08 +0300

    Рефакторинг

--- hw12_13_14_15_calendar/migrations/000001_initial_schema.down.sql
+++ hw12_13_14_15_calendar/migrations/000001_initial_schema.down.sql
@@ -1,1 +1,6 @@
-DROP TABLE users;
+-- Disable foreign key constraints
+SET CONSTRAINTS ALL DEFERRED;
+
+-- Drop tables
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS events;
--- hw12_13_14_15_calendar/migrations/000001_initial_schema.up.sql
+++ hw12_13_14_15_calendar/migrations/000001_initial_schema.up.sql
@@ -6,3 +6,17 @@ CREATE TABLE users
     id   UUID PRIMARY KEY,
     name VARCHAR(256)
 );
+
+CREATE TABLE events
+(
+    id          UUID PRIMARY KEY,
+    title       TEXT      NOT NULL,
+    start_dt    TIMESTAMP NOT NULL,
+    end_dt      TIMESTAMP NOT NULL,
+    description TEXT,
+    user_id     UUID      NOT NULL,
+    notify INTERVAL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+);
