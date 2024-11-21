c38a7ac86270348e8ac95ab8947b66cc58801099
Artur Khasanov <ah@self.team>
2024-11-21 19:10:47 +0300

    Тест

--- hw12_13_14_15_calendar/internal/server/http/handler_test.go
+++ hw12_13_14_15_calendar/internal/server/http/handler_test.go
@@ -17,11 +17,16 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-func TestHandler_createEvent(t *testing.T) {
+func TestHandler(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	requestDto := dto.CreateEventDto{
+	mockApp := mock_app.NewMockIApplication(ctrl)
+	server := &Server{app: mockApp}
+	mux := server.registerRoutes()
+
+	// Create
+	createEvenDto := dto.CreateEventDto{
 		UserID:      uuid.New(),
 		Title:       "Title",
 		Description: "Description",
@@ -29,27 +34,10 @@ func TestHandler_createEvent(t *testing.T) {
 		EndDt:       time.Now().Truncate(time.Second).Add(time.Hour),
 		NotifyAt:    time.Hour,
 	}
-	requestBody, _ := json.Marshal(requestDto)
-	responseModel := requestDto.ToModel()
-	responseJson, _ := json.Marshal(responseModel)
+	createdEvent := createEvenDto.ToModel()
 
-	mockApp := mock_app.NewMockIApplication(ctrl)
-	server := &Server{app: mockApp}
-	req, _ := http.NewRequest("POST", "/events", bytes.NewBuffer(requestBody))
-	rr := httptest.NewRecorder()
-	mux := server.registerRoutes()
-	mockApp.EXPECT().CreateEvent(gomock.Any(), requestDto).Return(responseModel, nil)
-	mux.ServeHTTP(rr, req)
-
-	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, string(responseJson), strings.TrimSpace(rr.Body.String()))
-}
-
-func TestHandler_updateEvent(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	requestDto := dto.UpdateEventDto{
+	// Update
+	updateEventDto := dto.UpdateEventDto{
 		ID:          uuid.New(),
 		UserID:      uuid.New(),
 		Title:       "New Title",
@@ -58,41 +46,81 @@ func TestHandler_updateEvent(t *testing.T) {
 		EndDt:       time.Now().Truncate(time.Second).Add(time.Hour),
 		NotifyAt:    time.Hour,
 	}
-	requestBody, _ := json.Marshal(requestDto)
-	responseModel := requestDto.ToModel()
-	responseJson, _ := json.Marshal(responseModel)
+	updatedEvent := updateEventDto.ToModel()
 
-	mockApp := mock_app.NewMockIApplication(ctrl)
-	server := &Server{app: mockApp}
-	req, _ := http.NewRequest("PUT", "/events/"+requestDto.ID.String(), bytes.NewBuffer(requestBody))
-	rr := httptest.NewRecorder()
-	mux := server.registerRoutes()
-	mockApp.EXPECT().UpdateEvent(gomock.Any(), requestDto).Return(responseModel, nil)
-	mux.ServeHTTP(rr, req)
-
-	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, string(responseJson), strings.TrimSpace(rr.Body.String()))
-}
-
-func TestHandler_deleteEvent(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	requestDto := dto.DeleteEventDto{
+	// Delete
+	deleteEventDto := dto.DeleteEventDto{
 		ID: uuid.New(),
 	}
-	requestBody, _ := json.Marshal(requestDto)
 
-	mockApp := mock_app.NewMockIApplication(ctrl)
-	server := &Server{app: mockApp}
-	req, _ := http.NewRequest("DELETE", "/events/"+requestDto.ID.String(), bytes.NewBuffer(requestBody))
-	rr := httptest.NewRecorder()
-	mux := server.registerRoutes()
-	mockApp.EXPECT().DeleteEvent(gomock.Any(), requestDto).Return(nil)
-	mux.ServeHTTP(rr, req)
+	tests := []struct {
+		name         string
+		method       string
+		url          string
+		requestDto   interface{}
+		mockCall     func()
+		expectedCode int
+		expectedBody string
+	}{
+		{
+			name:       "create event",
+			method:     "POST",
+			url:        "/events",
+			requestDto: createEvenDto,
+			mockCall: func() {
+				mockApp.EXPECT().
+					CreateEvent(gomock.Any(), createEvenDto).
+					Return(createdEvent, nil)
+			},
+			expectedCode: http.StatusOK,
+			expectedBody: func() string {
+				responseJson, _ := json.Marshal(createdEvent)
+				return string(responseJson)
+			}(),
+		},
+		{
+			name:       "update event",
+			method:     "PUT",
+			url:        "/events/" + updateEventDto.ID.String(),
+			requestDto: updateEventDto,
+			mockCall: func() {
+				mockApp.EXPECT().
+					UpdateEvent(gomock.Any(), updateEventDto).
+					Return(updatedEvent, nil)
+			},
+			expectedCode: http.StatusOK,
+			expectedBody: func() string {
+				responseJson, _ := json.Marshal(updatedEvent)
+				return string(responseJson)
+			}(),
+		},
+		{
+			name:       "delete event",
+			method:     "DELETE",
+			url:        "/events/" + deleteEventDto.ID.String(),
+			requestDto: deleteEventDto,
+			mockCall: func() {
+				mockApp.EXPECT().
+					DeleteEvent(gomock.Any(), deleteEventDto).
+					Return(nil)
+			},
+			expectedCode: http.StatusNoContent,
+			expectedBody: "",
+		},
+	}
 
-	assert.Equal(t, http.StatusNoContent, rr.Code)
-	assert.Empty(t, strings.TrimSpace(rr.Body.String()))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			requestBody, _ := json.Marshal(tt.requestDto)
+			req, _ := http.NewRequest(tt.method, tt.url, bytes.NewBuffer(requestBody))
+			rr := httptest.NewRecorder()
+			tt.mockCall()
+			mux.ServeHTTP(rr, req)
+
+			assert.Equal(t, tt.expectedCode, rr.Code)
+			assert.Equal(t, tt.expectedBody, strings.TrimSpace(rr.Body.String()))
+		})
+	}
 }
 
 func TestHandler_listEvents(t *testing.T) {
