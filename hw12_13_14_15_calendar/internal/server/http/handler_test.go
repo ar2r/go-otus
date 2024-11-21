ee539cc6fa0e881807cd07a5dd1fbad4d0cb6f77
Artur Khasanov <ah@self.team>
2024-11-21 18:12:56 +0300

    Тест

--- hw12_13_14_15_calendar/internal/server/http/handler_test.go
+++ hw12_13_14_15_calendar/internal/server/http/handler_test.go
@@ -20,10 +20,7 @@ func TestHandler_createEvent(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	mockApp := mock_app.NewMockIApplication(ctrl)
-	server := &Server{app: mockApp}
-
-	reqDto := dto.CreateEventDto{
+	requestDto := dto.CreateEventDto{
 		UserID:      uuid.New(),
 		Title:       "Title",
 		Description: "Description",
@@ -31,65 +28,18 @@ func TestHandler_createEvent(t *testing.T) {
 		EndDt:       time.Now().Truncate(time.Second).Add(time.Hour),
 		NotifyAt:    time.Hour,
 	}
+	requestBody, _ := json.Marshal(requestDto)
+	responseModel := requestDto.ToModel()
+	responseJson, _ := json.Marshal(responseModel)
 
-	reqBody, _ := json.Marshal(reqDto)
-	req, _ := http.NewRequest("POST", "/events", bytes.NewBuffer(reqBody))
-
+	mockApp := mock_app.NewMockIApplication(ctrl)
+	server := &Server{app: mockApp}
+	req, _ := http.NewRequest("POST", "/events", bytes.NewBuffer(requestBody))
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(server.createEventHandler)
-
-	model := reqDto.ToModel()
-	expectedJson, _ := json.Marshal(model)
-
-	mockApp.EXPECT().CreateEvent(gomock.Any(), reqDto).Return(model, nil)
-	handler.ServeHTTP(rr, req)
+	mux := server.registerRoutes()
+	mockApp.EXPECT().CreateEvent(gomock.Any(), requestDto).Return(responseModel, nil)
+	mux.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, string(expectedJson), strings.TrimSpace(rr.Body.String()))
+	assert.Equal(t, string(responseJson), strings.TrimSpace(rr.Body.String()))
 }
-
-//func TestUpdateEventHandler(t *testing.T) {
-//	ctrl := gomock.NewController(t)
-//	defer ctrl.Finish()
-//
-//	mockApp := NewMockIApplication(ctrl)
-//	server := &Server{app: mockApp}
-//
-//	reqDto := dto.UpdateEventDto{
-//		ID:    uuid.New(),
-//		Title: "Updated Event",
-//	}
-//	reqBody, _ := json.Marshal(reqDto)
-//	req, err := http.NewRequest("PUT", "/events/"+reqDto.ID.String(), bytes.NewBuffer(reqBody))
-//	assert.NoError(t, err)
-//
-//	rr := httptest.NewRecorder()
-//	handler := http.HandlerFunc(server.updateEventHandler)
-//
-//	mockApp.EXPECT().UpdateEvent(gomock.Any(), reqDto).Return(&dto.EventDto{ID: reqDto.ID}, nil)
-//
-//	handler.ServeHTTP(rr, req)
-//
-//	assert.Equal(t, http.StatusOK, rr.Code)
-//}
-//
-//func TestDeleteEventHandler(t *testing.T) {
-//	ctrl := gomock.NewController(t)
-//	defer ctrl.Finish()
-//
-//	mockApp := NewMockIApplication(ctrl)
-//	server := &Server{app: mockApp}
-//
-//	eventID := uuid.New()
-//	req, err := http.NewRequest("DELETE", "/events/"+eventID.String(), nil)
-//	assert.NoError(t, err)
-//
-//	rr := httptest.NewRecorder()
-//	handler := http.HandlerFunc(server.deleteEventHandler)
-//
-//	mockApp.EXPECT().DeleteEvent(gomock.Any(), dto.DeleteEventDto{ID: eventID}).Return(nil)
-//
-//	handler.ServeHTTP(rr, req)
-//
-//	assert.Equal(t, http.StatusNoContent, rr.Code)
-//}
